09cb74137ae717f34ce40f11e89feed15b5107a5
theo <boddaert.theo@free.fr>
2024-06-24 10:22:11 +0200

    echec

--- site/terminale/Principe_des_réseaux/Sécurisation_des_communications/src/enigma_machine.py
+++
@@ -1,9 +0,0 @@
-# Imporatation du module
-from enigma.machine import EnigmaMachine
-
-# Machine enigma configurée avec les rotors III,IV et II, le réflecteur C
-# et le tableau de connexions CE DV IO NR ST QX AP
-machine = EnigmaMachine.from_key_sheet(rotors = "III IV II",
-                                       reflector = "C",
-                                       plugboard_settings = "CE DV IO NR ST QX AP")
-
